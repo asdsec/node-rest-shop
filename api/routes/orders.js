783c28eb73d2429995019e64e43bb38c5a15bf70
sametdmr <sametdemir1049@gmail.com>
2022-07-17 16:45:58 +0300

    Step 2

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,30 +1,35 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Orders were fetched",
-  });
+router.get('/', (req, res, next) => {
+	res.status(200).json({
+		message: 'Orders were fetched',
+	});
 });
 
-router.post("/", (req, res, next) => {
-  res.status(201).json({
-    message: "Order was created",
-  });
+router.post('/', (req, res, next) => {
+	const order = {
+		productId: req.body.productId,
+		quantity: req.body.quantity,
+	};
+	res.status(201).json({
+		message: 'Order was created',
+		order: order,
+	});
 });
 
-router.get("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "Orders details",
-    orderId: req.params.orderId,
-  });
+router.get('/:orderId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Orders details',
+		orderId: req.params.orderId,
+	});
 });
 
-router.delete("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "Orders deleted",
-    orderId: req.params.orderId,
-  });
+router.delete('/:orderId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Orders deleted',
+		orderId: req.params.orderId,
+	});
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,51 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const productsRoutes = require("./api/routes/products");
-const ordersRoutes = require("./api/routes/orders");
+const productsRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
 
-app.use("/products", productsRoutes);
-app.use("/orders", ordersRoutes);
+mongoose
+	.connect('mongodb://localhost:27017/shop', {
+		useNewUrlParser: true,
+	})
+	.catch((err) => console.log(err));
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+		return res.status(200).json({});
+	}
+	next();
+});
+
+app.use('/products', productsRoutes);
+app.use('/orders', ordersRoutes);
+
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message,
+		},
+	});
+});
 
 module.exports = app;
